6d70d97a0f44bc25db221355262cb0c21bbc3674
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2015-02-27 14:56:44 +0200

    fixing SSO login redirect issue

--- modules/sso_jwt/login.php
+++ modules/sso_jwt/login.php
@@ -17,7 +17,12 @@ $handler->init();
 if( eZUser::currentUser()->isLoggedIn() === false ) {
     SSOJWTLogItem::create( $handler->getServiceProvider(), 'Redirect to login page' );
     eZHTTPTool::instance()->setSessionVariable( 'RedirectAfterLogin', 'sso_jwt/login/' . $handler->getServiceProvider() );
-    return $module->redirectTo( 'user/login?service_provider=' . $handler->getServiceProvider() );
+
+    $url = 'user/login?service_provider=' . $handler->getServiceProvider();
+    eZURI::transformURI( $url );
+
+    header( 'Location: ' . $url );
+    eZExecution::cleanExit();
 }
 
 $token = $handler->getToken();
